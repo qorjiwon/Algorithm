72e601c02bc3135234283ed1027cf472d6b47845
qorjiwon <wldnjsl2001@khu.ac.kr>
2023-06-04 22:35:29 +0900

    [Silver I] Title: 경로 찾기, Time: 164 ms, Memory: 34292 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,27 @@
+N = int(input())
+
+graph = []
+for _ in range(N):
+    graph.append(list(map(int, input().split())))
+
+from collections import deque
+
+result = [[0 for _ in range(N)] for _ in range(N)]
+queue = deque()
+
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == 1:
+            queue.append(j)
+            result[i][j]=1
+    while queue:
+        v = queue.popleft()
+        for j in range(N):
+            if graph[v][j] == 1 and result[i][j] == 0:
+                queue.append(j)
+                result[i][j] = 1
+
+for a in result:
+    for b in a:
+        print(b, end = ' ')
+    print()
