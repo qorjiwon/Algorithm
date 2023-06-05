74f0349ff99bed6856a28e875da9dce5e74a2d88
qorjiwon <wldnjsl2001@khu.ac.kr>
2023-06-05 20:05:15 +0900

    [Bronze I] Title: 이진수 덧셈, Time: 140 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2729. 이진수 덧셈/이진수 덧셈.py
+++ 백준/Bronze/2729. 이진수 덧셈/이진수 덧셈.py
@@ -3,10 +3,6 @@ input = sys.stdin.readline
 
 for _ in range(int(input())):
     a, b = input().split()
-    a_ = 0
-    b_ = 0
-    for i in range(len(a)):
-        a_ += int(a[len(a)-i-1])*pow(2, i)
-    for i in range(len(b)):
-        b_ += int(b[len(b)-i-1])*pow(2, i)
-    print(bin(a_+b_)[2:])
+    a = int(a, 2)
+    b = int(b, 2)
+    print(bin(a+b)[2:])
