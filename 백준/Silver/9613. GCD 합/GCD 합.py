6340d34c58d392f5e88ffbb322c8d922dd49d286
qorjiwon <wldnjsl2001@khu.ac.kr>
2023-06-05 21:58:29 +0900

    [Silver IV] Title: GCD 합, Time: 112 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/9613. GCD 합/GCD 합.py
+++ 백준/Silver/9613. GCD 합/GCD 합.py
@@ -1,20 +1,19 @@
 import sys
 input = sys.stdin.readline
 
+def gcd(a, b):
+    if a > b:
+        a, b = b, a
+    if a == 0:
+        return b
+    else:
+        return gcd(b % a, a)
+    
 for _ in range(int(input())):
     result = 0
     arr = list(map(int, input().split()))
     n = arr[0]
     for a in range(1,n):
         for b in range(a+1,n+1):
-            GCD = 1
-            if arr[a] < arr[b]:
-                GCD = arr[a]
-                while arr[a]%GCD + arr[b]%GCD != 0:
-                    GCD -= 1
-            else:
-                GCD = arr[b]
-                while arr[a]%GCD + arr[b]%GCD != 0:
-                    GCD -= 1
-            result += GCD
+            result += gcd(arr[a],arr[b])
     print(result)
