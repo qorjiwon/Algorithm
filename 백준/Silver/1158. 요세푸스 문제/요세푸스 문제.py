979166b0f300feaa7eef57c9406fb34cac5d687d
Jiwon Baek <wldnjsl2001@naver.com>
2023-06-07 23:04:27 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -4,9 +4,7 @@ N, K = map(int,sys.stdin.readline().split())
 a = list(range(1,N+1))
 ans = []
 i = 0
-
 while a:
     i = (i+K-1)%len(a)
-    ans.append(str(a.pop(i)))
-
-print('<'+', '.join(ans)+'>')
+    ans.append(a.pop(i))
+print('<'+', '.join(map(str,ans))+'>')
