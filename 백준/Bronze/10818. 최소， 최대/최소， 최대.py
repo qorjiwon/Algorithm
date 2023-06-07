60ac82128b7aba426871a9090fdedef830dcf04b
Jiwon Baek <wldnjsl2001@naver.com>
2023-06-07 23:15:46 +0900

    [Bronze III] Title: 최소, 최대, Time: 376 ms, Memory: 210772 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,6 +1,5 @@
-from sys import stdin
-input = stdin.readline
+import sys
 
-N = int(input())
-a = list(map(int, input().split()))
+sys.stdin.readline()
+a = [int(e) for e in sys.stdin.readline().split()]
 print(min(a), max(a))
