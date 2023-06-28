c66913e4e0da8ba4008f66be60c8b1d5fd323ef6
Jiwon Baek <wldnjsl2001@naver.com>
2023-06-28 12:49:27 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 248 ms, Memory: 128992 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -1,6 +1,6 @@
 import sys
 input = sys.stdin.readline
-inf = int(1e9)
+inf = 800*200000
 
 N, E = map(int,input().split())
 
@@ -10,12 +10,12 @@ dist1 = [inf]*(N+1)
 dist2 = [inf]*(N+1)
 dist3 = [inf]*(N+1)
 
-for _ in range(E):
+for _ in range(E): # 간선 추가
     a, b, c = map(int, input().split())
     graph[a].append((b,c))
     graph[b].append((a,c))
 
-def get_smallest_node(distance, visited):
+def get_smallest_node(distance, visited): # 방문하지 않는 노드 중 가장 가까운 노드의 인덱스 반환
     min_value = inf
     index = 0
     for i in range(1,N+1):
@@ -24,7 +24,7 @@ def get_smallest_node(distance, visited):
             index = i
     return index
 
-def dijkstra(start,distance):
+def dijkstra(start,distance): #start에서 각 노드까지의 최단 거리 반환
     distance[start]=0
     visited = [0 for _ in range(N+1)]
     visited[start]=1
@@ -39,10 +39,12 @@ def dijkstra(start,distance):
                 distance[j[0]] = cost
                 
 v1, v2 = map(int, input().split())
+
 dijkstra(1, dist1)
 dijkstra(v1, dist2)
 dijkstra(v2, dist3)
 
+# 경유 노드 순서에 따라서 dist 순서 바꿔줘야 되는거 잊지 말기
 result = min(dist1[v1]+dist2[v2]+dist3[N], dist1[v2]+dist3[v1]+dist2[N])
 
 if result >= inf:
