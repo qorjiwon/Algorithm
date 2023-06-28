f29bcaeaebe383cdfa2b8f24b9210443a5510598
Jiwon Baek <wldnjsl2001@naver.com>
2023-06-28 15:16:57 +0900

    [Gold III] Title: 파티, Time: 3580 ms, Memory: 142572 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,7 +1,7 @@
 from heapq import heappush, heappop
 import sys
 input = sys.stdin.readline
-inf=int(1e9)
+inf=100000
 
 N, M, X = map(int, input().split()) #마을과 학생 수, 간선 수, 파티 장소
 graph = [[] for _ in range(N+1)]
@@ -31,18 +31,12 @@ def dijkstra(start, distance):
                     distance[j[0]] = cost
                     heappush(q,(cost, v[2], j[0]))
                     
+
 dist=[inf]*(N+1)
 dijkstra(X, dist)
-distt = [inf]*(N+1)
-dijkstra(1, distt)
-result = distt[X]+dist[1]
+result = 0
 
-for i in range(2,X):
-    disttt = [inf]*(N+1)
-    dijkstra(i,disttt)
-    result = max(result, disttt[X]+dist[i])
-    
-for i in range(X+1,N+1):
+for i in range(1,N+1):
     disttt = [inf]*(N+1)
     dijkstra(i,disttt)
     result = max(result, disttt[X]+dist[i])
