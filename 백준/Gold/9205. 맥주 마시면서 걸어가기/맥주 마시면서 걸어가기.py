3aba82485cf8727dab0507b10913ff0ce410fdfc
Jiwon Baek <wldnjsl2001@naver.com>
2023-07-06 11:55:04 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 332 ms, Memory: 116984 KB -BaekjoonHub

---
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+inf = int(1e9)
+
+for _ in range(int(input())):
+    n = int(input())
+    location = [list(map(int, input().split())) for _ in range(n+2)]
+    graph = [[[inf] for i in range(n+2)] for _ in range(n+2)]
+    for i in range(n+2):
+        for j in range(n+2):
+            distance = abs(location[i][0]-location[j][0])+abs(location[i][1]-location[j][1])
+            if distance > 1000:
+                graph[i][j]=inf
+            else:
+                graph[i][j]=distance
+    for k in range(n+2):
+        for i in range(n+2):
+            for j in range(n+2):
+                graph[i][j]=min(graph[i][j], graph[i][k]+graph[k][j])
+    if graph[0][n+1] < inf:
+        print("happy")
+    else:
+        print('sad')
