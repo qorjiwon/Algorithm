6029791abcbdc8f81237d0a71604b0c2d3c2338f
Jiwon Baek <wldnjsl2001@naver.com>
2023-07-17 23:08:57 +0900

    [unrated] Title: 뒤에서 5등까지, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.py
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.py
@@ -1,7 +1,2 @@
-def solution(num_list):
-    answer = []
-    for _ in range(5):
-        m = min(num_list)
-        answer.append(m)
-        num_list.remove(m)
-    return answer
+def solution(n):
+    return sorted(n)[:5]
