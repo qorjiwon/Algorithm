54dba27ce1ad61f0e027b1e5136d15d4b5af4f31
Jiwon Baek <wldnjsl2001@naver.com>
2023-07-27 23:24:54 +0900

    Update 단지번호붙이기.py

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,40 +1,34 @@
-from collections import deque
-
-def bfs(m, i, j, visited):
-    queue = deque([[i, j]])
-    t=0
-    while queue:
-        v = queue.popleft()
-        t+=1
-        visited[v[0]][v[1]]=1
-        if v[0]-1 > -1 and visited[v[0]-1][v[1]]==0 and m[v[0]-1][v[1]]=='1':
-            queue.append([v[0]-1, v[1]])
-            visited[v[0]-1][v[1]]=1
-        if v[1]-1 > -1 and visited[v[0]][v[1]-1]==0 and m[v[0]][v[1]-1]=='1':
-            queue.append([v[0],v[1]-1])
-            visited[v[0]][v[1]-1]=1
-        if v[0]+1 < N and visited[v[0]+1][v[1]]==0 and m[v[0]+1][v[1]]=='1':
-            queue.append([v[0]+1, v[1]])
-            visited[v[0]+1][v[1]]=1
-        if v[1]+1 < N and visited[v[0]][v[1]+1]==0 and m[v[0]][v[1]+1]=='1':
-            queue.append([v[0], v[1]+1])
-            visited[v[0]][v[1]+1]=1
-    return t
+import sys
+input = sys.stdin.readline
 
 N = int(input())
-m = []
-for _ in range(N):
-    m.append(list(input()))
+m = [list(map(int,input().rstrip())) for _ in range(N)]
+dx=[1,0,-1,0]
+dy=[0,1,0,-1]
 
-danzi=[]
-visited=[[0 for _ in range(N)] for _ in range(N)]
+from collections import deque
+def bfs(x,y):
+    q = deque([(x,y)])
+    m[x][y]=0
+    ans=1
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if -1 < nx and nx < N and -1 < ny and ny < N and m[nx][ny]==1:
+                q.append((nx,ny))
+                m[nx][ny]=0
+                ans += 1
+    return ans
 
+result = []
 for i in range(N):
     for j in range(N):
-        if visited[i][j]==0 and m[i][j]=='1':
-            danzi.append(bfs(m, i, j, visited))
-
-print(len(danzi))
-danzi.sort()
-for i in danzi:
-    print(i)
+        if m[i][j] == 1:
+            result.append(bfs(i, j))
+            
+print(len(result))
+result.sort()
+for e in result:
+    print(e)
