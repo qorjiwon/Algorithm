b2cb6f3e8ccff51637617eed706cf8669b845ae9
Jiwon Baek <wldnjsl2001@naver.com>
2023-07-29 14:45:37 +0900

    [Silver I] Title: 음식물 피하기, Time: 176 ms, Memory: 118072 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
@@ -12,25 +12,22 @@ dy = [0,1,0,-1]
 
 from collections import deque
 q = deque()
-def bfs(x, y):
-    q.append((x,y))
-    arr[x][y]=0
-    ans = 1
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if -1 < nx and nx < N and -1 < ny and ny < M and arr[nx][ny]==1:
-                q.append((nx,ny))
-                arr[nx][ny]=0
-                ans += 1
-    return ans
-
 result = 0
 for j in range(M):
     for i in range(N):
         if arr[i][j]==1:
-            result = max(result, bfs(i,j))
+            q.append((i,j))
+            arr[i][j]=0
+            ans = 1
+            while q:
+                x, y = q.popleft()
+                for i in range(4):
+                    nx = x + dx[i]
+                    ny = y + dy[i]
+                    if -1 < nx and nx < N and -1 < ny and ny < M and arr[nx][ny]==1:
+                        q.append((nx,ny))
+                        arr[nx][ny]=0
+                        ans += 1
+            result = max(result, ans)
             
 print(result)
