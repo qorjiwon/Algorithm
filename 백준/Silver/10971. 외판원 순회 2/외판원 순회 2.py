ad0f917f0db9afa386ed67d728f92a1c6026e93e
Jiwon Baek <wldnjsl2001@naver.com>
2023-07-30 18:00:25 +0900

    [Silver II] Title: 외판원 순회 2, Time: 744 ms, Memory: 320136 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -5,11 +5,8 @@ N = int(input())
 traffic = [list(map(int, input().split())) for _ in range(N)]
 
 from collections import deque
-q=deque()
+q=deque([([0,i],1,traffic[0][i]) for i in range(1,N) if traffic[0][i] != 0])
 distance = []
-for i in range(1,N): # 0번째 도시에서 출발
-    if traffic[0][i] != 0:
-        q.append(([0,i],1,traffic[0][i]))
 while q:
     arr, n, d = q.popleft()
     if n == N-1:
