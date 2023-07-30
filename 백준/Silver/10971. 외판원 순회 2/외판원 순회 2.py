6447188f22c7d8bdcbafc118d1262ec53f1ee2b5
Jiwon Baek <wldnjsl2001@naver.com>
2023-07-30 16:30:04 +0900

    [Silver II] Title: 외판원 순회 2, Time: 744 ms, Memory: 320136 KB -BaekjoonHub

---
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+traffic = [list(map(int, input().split())) for _ in range(N)]
+
+from collections import deque
+q=deque()
+distance = []
+for i in range(1,N): # 0번째 도시에서 출발
+    if traffic[0][i] != 0:
+        q.append(([0,i],1,traffic[0][i]))
+while q:
+    arr, n, d = q.popleft()
+    if n == N-1:
+        last = arr[n]
+        if traffic[last][0] != 0:
+            distance.append(d+traffic[last][0])
+    else:
+        s = arr[n]
+        for i in range(1,N):
+            if i not in arr and traffic[s][i] != 0:
+                q.append((arr+[i],n+1, d+traffic[s][i]))
+print(min(distance))
