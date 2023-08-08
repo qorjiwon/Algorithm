ba0ba92da172c4d61855aa4bfa9ec0ee86a5f767
Jiwon Baek <wldnjsl2001@naver.com>
2023-08-09 02:37:17 +0900

    [Gold II] Title: 교환, Time: 136 ms, Memory: 115004 KB -BaekjoonHub

--- 백준/Gold/1039. 교환/교환.py
+++ 백준/Gold/1039. 교환/교환.py
@@ -1,23 +1,17 @@
-import sys
-input = sys.stdin.readline
+N, K = input().split()
+K = int(K) ; M = len(N) ; N = tuple(N)
 
-N, K = input().split() ; K = int(K) ; M = len(N)
-N = tuple(N)
-
-if M == 1:
-    print(-1)
-elif M == 2 and N[1]=='0':
-    print(-1)
-else:
-    arr = [N]
-    from itertools import combinations
-    for _ in range(K):
-        T = set()
-        dic = list(combinations(range(M),2))
-        for n in arr:
-            for a, b in dic:
-                newn = list(n)
-                newn[a], newn[b] = newn[b], newn[a]
-                T.add(tuple(newn))
-        arr = sorted(list(T))
-    print("".join(arr[-1]))
+if M == 1: print(-1) ; exit()
+if M == 2 and N[1]=='0': print(-1) ; exit()
+arr = [N]
+from itertools import combinations
+for _ in range(K):
+    T = set()
+    dic = list(combinations(range(M),2))
+    for n in arr:
+        for a, b in dic:
+            newn = list(n)
+            newn[a], newn[b] = newn[b], newn[a]
+            T.add(tuple(newn))
+    arr = sorted(list(T))
+print(''.join(arr[-1]))
