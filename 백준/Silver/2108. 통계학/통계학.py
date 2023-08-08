309daeeef52713df8935c649650744e67993b83b
Jiwon Baek <wldnjsl2001@naver.com>
2023-08-08 22:18:29 +0900

    [Silver III] Title: 통계학, Time: 308 ms, Memory: 123916 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = [int((input())) for _ in range(N)]
+
+if N == 1:
+    for _ in range(3):
+        print(arr[0])
+    print(0)
+else:
+    print(round(sum(arr)/N)) # 산술평균
+    arr.sort()
+    print(arr[N//2]) # 중앙값
+    cnt={} # 빈도 수 구하기
+    for i in arr:
+        if i in cnt:
+            cnt[i]+=1
+        else:
+            cnt[i]=1
+    mx = max(cnt.values())
+    mxcnt = [e for e in cnt if cnt[e] == mx]
+    mxcnt.sort()
+    if len(mxcnt) == 1: # 최빈값이 하나일 때
+        print(mxcnt.pop())
+    else: # 최빈값이 여러 개일 때
+        print(mxcnt[1])
+    print(arr[N-1]-arr[0]) # 범위
