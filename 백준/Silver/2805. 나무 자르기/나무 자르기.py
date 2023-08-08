0c58557143c6bfa2209b96e504b189b3fc1ff8d6
Jiwon Baek <wldnjsl2001@naver.com>
2023-08-09 03:17:26 +0900

    [Silver II] Title: 나무 자르기, Time: 4532 ms, Memory: 143400 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -15,7 +15,7 @@ while min_high <= max_high:
             s += t-high
     if s < M: # 설정 높이가 너무 높을 때
         max_high = high-1
-    elif s >= M: # 설정 높이가 너무 낮을 때
+    elif s >= M: # 일단 만족. 설정 높이가 너무 낮을 수도
         min_high = high+1
         
 print(max_high)
