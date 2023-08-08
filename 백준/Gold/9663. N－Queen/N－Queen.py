74ddab786b6952c201a096c19c4bf339056fabb3
Jiwon Baek <wldnjsl2001@naver.com>
2023-08-08 22:26:12 +0900

    [Gold IV] Title: N-Queen, Time: 30804 ms, Memory: 216484 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -7,23 +7,18 @@ result = 0
 
 def queens(i):
     global result
-    if promising(i): # 유망하면
-        if i == N:
-            result += 1
-        else:
-            for j in range(N):
-                col[i+1] = j
-                queens(i+1)
-                
-def promising(i):
-    k = 1
-    while k < i:
+    for k in range(1,i):
         if col[i]==col[k] or abs(col[i]-col[k]) == i-k:
             # i번째 행과 k번째 행에서의 queen의 위치가 같은 열이거나
             # 대각선 방향에 있으면
             return 0 # 유망하지 않음
-        k += 1
-    return 1 # 유망함
+     # 유망하면
+    if i == N:
+        result += 1
+    else:
+        for j in range(N):
+            col[i+1] = j
+            queens(i+1)
 
 queens(0)
 print(result)
