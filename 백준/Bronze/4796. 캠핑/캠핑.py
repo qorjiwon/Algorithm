8d07b83f8d66fb2cd5c05e190dd1eaf84b1afcaf
Jiwon Baek <wldnjsl2001@naver.com>
2023-09-04 15:00:17 +0900

    [Bronze I] Title: 캠핑, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/4796. 캠핑/캠핑.py
+++ 백준/Bronze/4796. 캠핑/캠핑.py
@@ -4,9 +4,9 @@ input = sys.stdin.readline
 i = 1
 while 1:
     L, P, V = map(int, input().split())
-    if L == 0:
+    if not L:
         break
     ans = (V//P)*L
     ans += min(V%P, L)
-    print('Case ',i,': ',ans,sep='')
+    print(f"Case {i}: {ans}")
     i += 1
