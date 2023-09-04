e81f4956a5bedd009071b6d95187aa8b1e144b0f
Jiwon Baek <wldnjsl2001@naver.com>
2023-09-04 14:40:39 +0900

    [Silver IV] Title: 설탕 배달, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+for i in range(N//5+1):
+    if (N%5+5*i)%3 == 0:
+        ans = N//5-i
+        ans += (N%5+5*i)//3
+        print(ans)
+        exit()
+print(-1)
