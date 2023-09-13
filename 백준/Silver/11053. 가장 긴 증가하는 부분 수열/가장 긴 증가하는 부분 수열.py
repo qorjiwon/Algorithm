079602a584f38b8ae7c5b1547590d7bb1646fac2
Jiwon Baek <wldnjsl2001@naver.com>
2023-09-14 00:38:40 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 132 ms, Memory: 114488 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -6,7 +6,7 @@ A = list(map(int,input().split()))
 dp = [1]*N
 for i in range(N):
     m = A[i]
-    for k in range(i-1,-1,-1):        
+    for k in range(i):        
         if m > A[k]:
             dp[i] = max(dp[i], dp[k]+1)
 print(max(dp))
