9800981519a05b94461c8eddea4905cead2d9a60
Jiwon Baek <wldnjsl2001@naver.com>
2023-09-18 20:14:36 +0900

    [Silver III] Title: 2×n 타일링, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -2,12 +2,9 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-dp = [0]*max(n+1,3)
-dp[0] = 0
-dp[1] = 1
-dp[2] = 2
+dp = [1, 2]
 
-for i in range(3, n+1):
-    dp[i] = dp[i-1]+dp[i-2]
+for i in range(2, n+1):
+    dp.append(dp[i-1]+dp[i-2])
 
-print(dp[n]%10007)
+print(dp[n-1]%10007)
