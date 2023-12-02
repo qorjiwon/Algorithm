7c349774c75eac71a078896fe74e8ab12d86d6a2
Jiwon Baek <wldnjsl2001@naver.com>
2023-12-02 22:42:51 +0900

    [level unrated] Title: a와 b 출력하기, Time: 42.61 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.js
+++ 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.js
@@ -1,12 +1,12 @@
 const readline = require('readline');
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+    input: process.stdin
 });
 
 let input = [];
 
 rl.on('line', function (line) {
-    const [a, b] = line.split(' ');
-    console.log("a =", a, "\nb =", b);
-})
+    input = line.split(' ');
+}).on('close', function () {
+    console.log("a =",Number(input[0]), "\nb =",Number(input[1]));
+});
