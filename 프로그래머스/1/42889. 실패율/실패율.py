b410426e6c1a1a14e0aca173df048bfcd6b13d3e
백지원 Jiwon Baek <wldnjsl2001@khu.ac.kr>
2025-03-29 15:09:45 +0900

    [level 1] Title: 실패율, Time: 2979.99 ms, Memory: 14.8 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,8 +1,12 @@
 def solution(N, stages):
-    cnt_user = [0]*(N+2) # 각 스테이지별 클리어한 사용자 수 배열
-    for stage in stages:
-        for i in range(1, stage+1):
-            cnt_user[i] += 1
+    stages.sort()
+    cnt_user = [len(stages)]*(N+2) # 각 스테이지별 도달한 사용자 수 배열
+    notyet = 0 # 아직 도달 못한 사용자 수
+    
+    for i in range(1, N+1):
+        cnt_user[i] -= notyet
+        notyet += stages.count(i)
+        
     rates = [[i, stages.count(i)/cnt_user[i] if cnt_user[i] != 0 else 0] for i in range(1, N+1)]
     rates = sorted(rates, key=lambda x:x[1], reverse=True)
     answer = [rate[0] for rate in rates]
