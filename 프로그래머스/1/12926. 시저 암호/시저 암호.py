675ab677adc1fb81d33920117296c85ec094d906
백지원 Jiwon Baek <wldnjsl2001@khu.ac.kr>
2025-04-07 01:50:08 +0900

    [level 1] Title: 시저 암호, Time: 1.58 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,4 +1,12 @@
 def solution(s, n):
-    print(ord('z'), ord('a')-ord('b'), chr(ord('a')),chr(ord('a')+1), (ord('z')+1)%ord('a'))
-    answer = ''.join([' ' if i == ' ' else ( chr( (ord(i)+n-ord('A'))%26+ord('A') ) if i < 'a' else chr( (ord(i)+n-ord('a'))%26+ord('a') ) ) for i in list(s)])
-    return answer
+    # ord('a')=97, ord('z')=122
+    # ord('A')=65, ord('Z')=90
+    n = n%26
+    return  ''.join(
+        [ ch if ch == ' ' 
+         else (
+             ( chr(96 + ord(ch)+n-122) if ord(ch)+n > 122 else chr(ord(ch)+n) ) if ord(ch) > 90 # 소문자
+             else ( chr(64 + ord(ch)+n-90) if ord(ch)+n > 90 else chr(ord(ch)+n) ) # 대문자
+         )
+         for ch in s
+        ])
