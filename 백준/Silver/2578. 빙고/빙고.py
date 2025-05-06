21feeae52ae3d8655b7410a20977eea33f633391
백지원 Jiwon Baek <wldnjsl2001@khu.ac.kr>
2025-05-06 20:55:43 +0900

    [Silver IV] Title: 빙고, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.py
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -11,25 +11,24 @@ for _ in range(5):
 
 def bingo(m):
     num = 0
-    # check rows
+    # rows
     for i in range(5):
-        if m[i][0] == 0 and m[i][1] == 0 and m[i][2] == 0 and m[i][3] == 0 and m[i][4] == 0:
+        if sum(m[i]) == 0:
             num += 1
-    # check columns
+    # columns
     for i in range(5):
-        if m[0][i] == 0 and m[1][i] == 0 and m[2][i] == 0 and m[3][i] == 0 and m[4][i] == 0:
+        if sum([m[k][i] for k in range(5)]) == 0:
             num += 1
-    # check diagonals
-    if m[0][0] == 0 and m[1][1] == 0 and m[2][2] == 0 and m[3][3] == 0 and m[4][4] == 0:
+    # diagonals
+    if sum([m[i][i] for i in range(5)]) == 0:
         num += 1
-    if m[4][0] == 0 and m[3][1] == 0 and m[2][2] == 0 and m[1][3] == 0 and m[0][4] == 0:
+    if sum([m[i][4-i] for i in range(5)]) == 0:
         num += 1
     return num
 
-ans = 0
+ans = 1
 for low in m2:
     for e in low:
-        ans += 1
         for i in range(5):
             for j in range(5):
                 if m1[i][j] == e:
@@ -37,3 +36,4 @@ for low in m2:
         if bingo(m1) >= 3:
             print(ans)
             sys.exit(0)
+        ans += 1
