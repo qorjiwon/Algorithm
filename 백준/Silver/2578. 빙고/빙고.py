347d1f3003e3b48172a6548f080ddcdf58242722
백지원 Jiwon Baek <wldnjsl2001@khu.ac.kr>
2025-05-06 20:49:42 +0900

    [Silver IV] Title: 빙고, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.py
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -1,68 +1,39 @@
-chulsu = []
-for i in range(5):
-    chulsu.append(list(map(int, input().split())))
-    
-dae=[[],[]]
-for i in range(5):
-    dae[0].append(chulsu[i][i])
-for i in range(5):
-    dae[1].append(chulsu[i][4-i])
-    
-ga=[[],[],[],[],[]]
-for i in range(5):
-    for j in range(5):
-        ga[i].append(chulsu[i][j])
+import sys
+input = sys.stdin.readline
 
-sae =[[],[],[],[],[]]
+m1 = []
+m2 = []
 
-for i in range(5):
-    for j in range(5):
-        sae[i].append(chulsu[j][i])
+for _ in range(5):
+    m1.append(list(map(int, input().split())))
+for _ in range(5):
+    m2.append(list(map(int, input().split())))
 
+def bingo(m):
+    num = 0
+    # check rows
+    for i in range(5):
+        if m[i][0] == 0 and m[i][1] == 0 and m[i][2] == 0 and m[i][3] == 0 and m[i][4] == 0:
+            num += 1
+    # check columns
+    for i in range(5):
+        if m[0][i] == 0 and m[1][i] == 0 and m[2][i] == 0 and m[3][i] == 0 and m[4][i] == 0:
+            num += 1
+    # check diagonals
+    if m[0][0] == 0 and m[1][1] == 0 and m[2][2] == 0 and m[3][3] == 0 and m[4][4] == 0:
+        num += 1
+    if m[4][0] == 0 and m[3][1] == 0 and m[2][2] == 0 and m[1][3] == 0 and m[0][4] == 0:
+        num += 1
+    return num
 
-sa = []
-for i in range(5):
-    sa.append(list(map(int, input().split())))
-    
-result = 1
-for i in range(5):
-    gameover = 0
-    for j in range(5):
-        bingo = 0
-        
-        for k in range(2):
-            for r in range(len(dae[k])):
-                if (sa[i][j]==dae[k][r]):
-                    dae[k].pop(r)
-                    break
-                    
-        for k in range(5):
-            for r in range(len(ga[k])):
-                if(sa[i][j] == ga[k][r]):
-                    ga[k].pop(r)
-                    break
-            for r in range(len(sae[k])):
-                if(sa[i][j] == sae[k][r]):
-                    sae[k].pop(r)
-                    break
-            
-        for k in range(2):
-            if(len(dae[k])==0):
-                bingo += 1
-        for k in range(5):
-            if (len(ga[k])==0):
-                bingo += 1
-            if (len(sae[k])==0):
-                bingo += 1
-            
-        if (bingo >2):
-            gameover = 1
-            break
-        else:
-            result += 1
-        
-    if (gameover):
-        break
-            
-
-print(result)
+ans = 0
+for low in m2:
+    for e in low:
+        ans += 1
+        for i in range(5):
+            for j in range(5):
+                if m1[i][j] == e:
+                    m1[i][j] = 0
+        if bingo(m1) >= 3:
+            print(ans)
+            sys.exit(0)
