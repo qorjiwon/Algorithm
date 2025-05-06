efaaf52e9e1ae66eb06551c4e518249a6b05861e
백지원 Jiwon Baek <wldnjsl2001@khu.ac.kr>
2025-05-06 20:11:15 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.21 ms, Memory: 9.42 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,5 +1,4 @@
 import re
 
 def solution(s):
-    answer = True if (len(s) == 4 or len(s) == 6) and len(re.findall('[^0-9]', s))==0 else False
-    return answer
+    return True if re.fullmatch("[0-9]{4}|[0-9]{6}", s) else False
