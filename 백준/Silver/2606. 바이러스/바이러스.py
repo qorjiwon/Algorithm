af2405b5bbf8cc7c113f5891d4a138d1684a0d01
백지원 Jiwon Baek <wldnjsl2001@khu.ac.kr>
2025-05-27 00:31:48 +0900

    [Silver III] Title: 바이러스, Time: 56 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,22 +1,23 @@
-C = int(input())
-SS = int(input())
+import sys
+input = sys.stdin.readline
+from collections import deque
 
-graph=[[] for i in range(C+1)]
-#print(graph)
-for i in range(SS):
-    n, m = map(int, input().split())
-    graph[n].append(m)
-    graph[m].append(n)
+n = int(input())
+graph = [[] for _ in range(n + 1)]
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-visited=[0]*(C+1)
-#print(graph)
-
-def dfs(graph, v, visited):
-    visited[v]=1
-    for i in graph[v]:
-        if(visited[i] == 0):
-            dfs(graph, i, visited)
-
-dfs(graph, 1, visited)
-#최초 감염 컴퓨터 제외
-print(visited.count(1)-1)
+infected = [0]*(n + 1)
+infected[1] = 1
+q = deque([1])
+ans = 0
+while q:
+    computer = q.popleft()
+    for neighbor in graph[computer]:
+        if not infected[neighbor]:
+            q.append(neighbor)
+            infected[neighbor] = 1
+            ans += 1
+print(ans)
