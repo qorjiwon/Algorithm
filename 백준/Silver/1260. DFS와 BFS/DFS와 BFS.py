c592131d1e59a1e97e8f5a0f66a45f3a12e69407
백지원 Jiwon Baek <wldnjsl2001@khu.ac.kr>
2025-05-27 00:23:12 +0900

    [Silver II] Title: DFS와 BFS, Time: 68 ms, Memory: 35664 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,38 +1,35 @@
 import sys
 input = sys.stdin.readline
 
-N, M, V = map(int, input().split())
-graph = [[] for _ in range(N+1)]
-for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-    
-for i in range(N+1):
-    graph[i].sort()
-
-def dfs(start, visited):
-    visited[start]=1
-    print(start, end=' ')
-    for e in graph[start]:
-        if visited[e] == 0:
-            dfs(e, visited)
-    
 from collections import deque
 
-def bfs(visited, q):
-    while q:
-        x = q.popleft()
-        print(x, end=' ')
-        for e in graph[x]:
-            if visited[e] == 0:
-                q.append(e)
-                visited[e]=1
-    
-visited = [0]*(N+1)
-dfs(V, visited)
+N, M, V = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+visited = [False] * (N + 1)
+
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for i in range(1, N + 1):
+    graph[i].sort()
+
+def dfs(node):
+    print(node, end=' ')
+    visited[node] = True
+    for neighbor in graph[node]:
+        if not visited[neighbor]:
+            dfs(neighbor)
+dfs(V)
 print()
+visited = [False] * (N + 1)
 q = deque([V])
-visitedd = [0]*(N+1)
-visitedd[V]=1
-bfs(visitedd, q)
+while q:
+    node = q.popleft()
+    if not visited[node]:
+        visited[node] = True
+        print(node, end=' ')
+        for neighbor in graph[node]:
+            if not visited[neighbor]:
+                q.append(neighbor)
