78536176cf31e573f78578e6a6c360b0898bd160
백지원 Jiwon Baek <wldnjsl2001@khu.ac.kr>
2025-05-27 00:20:33 +0900

    [Silver I] Title: 음식물 피하기, Time: 68 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
@@ -1,33 +1,29 @@
 import sys
 input = sys.stdin.readline
 
-N, M, K = map(int, input().split())
-arr=[[0]*(M) for _ in range(N)]
-for _ in range(K):
-    a, b = map(int, input().split())
-    arr[a-1][b-1]=1
-    
-dx = [1,0,-1,0]
-dy = [0,1,0,-1]
-
 from collections import deque
-q = deque()
-result = 0
-for j in range(M):
-    for i in range(N):
-        if arr[i][j]==1:
-            q.append((i,j))
-            arr[i][j]=0
-            ans = 1
+
+N, M, K = map(int, input().split())
+m = [[0]*M for _ in range(N)]
+for _ in range(K):
+    r, c = map(int, input().split())
+    m[r-1][c-1] = 1
+
+ans = 0
+d = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+for i in range(N):
+    for j in range(M):
+        if m[i][j]:
+            m[i][j] = 0
+            size = 1
+            q = deque([(i, j)])
             while q:
                 x, y = q.popleft()
-                for i in range(4):
-                    nx = x + dx[i]
-                    ny = y + dy[i]
-                    if -1 < nx and nx < N and -1 < ny and ny < M and arr[nx][ny]==1:
-                        q.append((nx,ny))
-                        arr[nx][ny]=0
-                        ans += 1
-            result = max(result, ans)
-            
-print(result)
+                for dx, dy in d:
+                    nx, ny = x + dx, y + dy
+                    if 0 <= nx < N and 0 <= ny < M and m[nx][ny]:
+                        m[nx][ny] = 0
+                        q.append((nx, ny))
+                        size += 1
+            ans = max(ans, size)
+print(ans)
