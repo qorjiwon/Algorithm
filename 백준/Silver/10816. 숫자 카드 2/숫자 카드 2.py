c913f48052a7406416142b07dabe945daea41d48
백지원 Jiwon Baek <wldnjsl2001@khu.ac.kr>
2025-12-22 12:58:55 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 560 ms, Memory: 125196 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,14 +1,10 @@
+from collections import Counter
+
 N = int(input())
 nums = list(map(int, input().split()))
+nums = counter = Counter(nums)
 
 M = int(input())
 targets = list(map(int, input().split()))
 
-ans = {}
-for n in nums:
-    if n in ans:
-        ans[n] += 1
-    else:
-        ans[n] = 1
-
-print(' '.join([str(ans.get(t, 0)) for t in targets]))
+print(' '.join([str(nums.get(t, 0)) for t in targets]))
