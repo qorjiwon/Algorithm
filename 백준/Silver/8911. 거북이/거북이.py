dfd9ccee31a882a7b5ca970f209fbacda571d55f
백지원 Jiwon Baek <wldnjsl2001@khu.ac.kr>
2025-12-24 00:19:47 +0900

    [Silver III] Title: 거북이, Time: 2484 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/8911. 거북이/거북이.py
@@ -0,0 +1,23 @@
+d = [(-1, 0), (0, -1), (1, 0), (0, 1)] # 왼 위 오 아
+
+for _ in range(int(input())):
+    x, y = 0, 0
+    direction = 1
+    left, right, top, bottom = 0, 0, 0, 0
+    order = input()
+    for step in order:
+        if step == 'F':
+            x += d[direction][0]
+            y += d[direction][1]
+        elif step == 'B':
+            x -= d[direction][0]
+            y -= d[direction][1]
+        elif step == 'L':
+            direction = (direction - 1) % 4
+        elif step == 'R':
+            direction = (direction + 1) % 4
+        left = min(left, x)
+        right = max(right, x)
+        bottom = min(bottom, y)
+        top = max(top, y)
+    print((right - left) * (top - bottom))
