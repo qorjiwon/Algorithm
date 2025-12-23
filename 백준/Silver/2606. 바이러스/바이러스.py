bed634bf8dc0e706060903371bf2667ed650f323
백지원 Jiwon Baek <wldnjsl2001@khu.ac.kr>
2025-12-23 23:40:44 +0900

    [Silver III] Title: 바이러스, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,22 +1,21 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
 
 n = int(input())
 connection = [[] for _ in range(n+1)]
 infected = [0] * (n+1)
 infected[1] = 1
-q = deque([1])
+
 for _ in range(int(input())):
     a, b = map(int, input().split())
     connection[a].append(b)
     connection[b].append(a)
 
-while q:
-    x = q.pop()
+def dfs(x):
     for i in connection[x]:
         if not infected[i]:
             infected[i] = 1
-            q.append(i)
+            dfs(i)
 
+dfs(1)
 print(sum(infected)-1)
