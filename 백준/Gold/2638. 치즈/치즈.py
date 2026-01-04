6701426d60024d0eb76477af281a15810243f359
백지원 Jiwon Baek <wldnjsl2001@khu.ac.kr>
2026-01-04 14:31:26 +0900

    [Gold III] Title: 치즈, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -9,23 +9,23 @@ d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 ans = -1
 
 while q:
-    _q = [] # 다음에 돌 곳
-    while q:  # 치즈 녹을 곳 검사
+    _q = []
+    while q:
         x, y = q.pop()
         for dx, dy in d:
             nx, ny = x + dx, y + dy
             if 0 <= nx < N and 0 <= ny < M:
                 v = arr[nx][ny]
-                if v < 0: # 이미 방문
+                if v < 0:
                     continue
-                elif not v: # 빈 자리
+                elif not v:
                     arr[nx][ny] = -1
                     q.append((nx, ny))
                 elif v == 1:
                     arr[nx][ny] += 1
-                else: # v > 1, 한 번 방문했는데 재방문
+                else:
                     arr[nx][ny] = -1
-                    _q.append((nx, ny)) # 다음 turn(한 시간 후) 방문
+                    _q.append((nx, ny))
     q = _q
     ans += 1
 
