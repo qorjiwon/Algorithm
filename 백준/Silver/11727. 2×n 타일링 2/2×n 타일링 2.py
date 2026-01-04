6f18e2ff5977b65ab6fa8453eaf4bed7b82975b1
백지원 Jiwon Baek <wldnjsl2001@khu.ac.kr>
2026-01-04 19:49:41 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -1,12 +1,12 @@
 n = int(input())
 
-dp = [0] * (n + 1)
+dp = [0] * (1001)
 dp[1] = 1
 
 if n >= 2:
     dp[2] = 3
 
 for i in range(3, n + 1):
-    dp[i] = (dp[i-1] + dp[i-2] * 2)%10007
+    dp[i] = dp[i-1] + dp[i-2] * 2
 
-print(dp[n])
+print(dp[n]%10007)
