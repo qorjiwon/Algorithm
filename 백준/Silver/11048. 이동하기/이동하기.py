6a3851ad8753dd027dd52d49446b29643470a1d8
백지원 Jiwon Baek <wldnjsl2001@khu.ac.kr>
2026-01-05 23:48:29 +0900

    [Silver II] Title: 이동하기, Time: 2156 ms, Memory: 71692 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -0,0 +1,13 @@
+N, M = map(int, input().split())
+room = [list(map(int, input().split())) for _ in range(N)]
+
+d = [(-1, 0), (0, -1), (-1, -1)]
+for x in range(N):
+    for y in range(M):
+        candies = [room[x][y]]
+        for (dx, dy) in d:
+            nx, ny = x+dx, y+dy
+            if 0 <= nx < N and 0 <= ny < M and room[nx][ny]:
+                candies.append(room[x][y]+room[nx][ny])
+        room[x][y] = max(candies)
+print(room[N-1][M-1])
