5fc36685e08024927f0190c2dda505248cde45b8
백지원 Jiwon Baek <wldnjsl2001@khu.ac.kr>
2026-01-05 23:51:14 +0900

    [Silver II] Title: 이동하기, Time: 2068 ms, Memory: 71692 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-room = [list(map(int, input().split())) for _ in range(N)]
+room = [[*map(int, input().split())] for _ in range(N)]
 
 d = [(-1, 0), (0, -1), (-1, -1)]
 for x in range(N):
