a24b1e758e7db7d15c0108adae58feede3bea460
백지원 Jiwon Baek <wldnjsl2001@khu.ac.kr>
2026-03-07 02:13:35 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,7 @@
+SELECT BOARD_ID, WRITER_ID, TITLE, PRICE
+, case when status='done' then '거래완료'
+when status ='sale' then '판매중'
+else '예약중' end as STATUS
+from USED_GOODS_BOARD
+where CREATED_DATE = '2022-10-05'
+order by BOARD_ID desc
