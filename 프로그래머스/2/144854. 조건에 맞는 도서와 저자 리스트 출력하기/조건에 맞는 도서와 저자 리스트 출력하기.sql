507b40f80ec5bb59f3af1ffbf3ea99e04d0654c6
백지원 Jiwon Baek <wldnjsl2001@khu.ac.kr>
2026-03-07 00:12:41 +0900

    [level 2] Title: 조건에 맞는 도서와 저자 리스트 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -0,0 +1,4 @@
+SELECT BOOK_ID, AUTHOR_NAME, date_format(PUBLISHED_DATE,'%Y-%m-%d') as PUBLISHED_DATE
+from book b join author a on b.author_id = a.author_id
+where category = '경제'
+order by published_date
