2d0c24eb6addd144c28638a5c04bd6a5d3ede158
백지원 Jiwon Baek <wldnjsl2001@khu.ac.kr>
2026-03-06 20:49:17 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,4 +1,4 @@
 select FOOD_TYPE, REST_ID, REST_NAME, FAVORITES
-from (select *, rank() over (partition by food_type order by favorites desc) as rnk from REST_INFO) as m
+from (select *, rank() over (partition by food_type order by favorites desc) from REST_INFO) as m
 group by FOOD_TYPE
 order by FOOD_TYPE desc
