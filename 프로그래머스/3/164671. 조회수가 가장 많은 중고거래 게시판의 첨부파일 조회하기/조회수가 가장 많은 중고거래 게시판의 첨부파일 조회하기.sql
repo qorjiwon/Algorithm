f2e226adcf0f745874d3acb112e8981bd68b4509
백지원 Jiwon Baek <wldnjsl2001@khu.ac.kr>
2026-03-07 03:04:48 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,4 +1,4 @@
 SELECT concat('/home/grep/src/', b.BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT) as file_path
 from USED_GOODS_BOARD b join USED_GOODS_FILE f on b.BOARD_ID = f.BOARD_ID
-where b.board_id = (select board_id from USED_GOODS_BOARD a group by board_id order by views desc limit 1)
+where views = (select max(views) from USED_GOODS_BOARD)
 order by file_id desc
