b3ee0638d8667b7f4c20fa6ac3a9f591e2bbd500
백지원 Jiwon Baek <wldnjsl2001@khu.ac.kr>
2026-03-14 22:32:21 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,4 @@
+select MEMBER_ID, MEMBER_NAME, GENDER, date_format(DATE_OF_BIRTH, '%Y-%m-%d') as DATE_OF_BIRTH
+from MEMBER_PROFILE 
+where month(DATE_OF_BIRTH) = 3 and TLNO is not null and gender='W'
+order by MEMBER_ID
